4d770aa5bc6a38a3786f7ed0a9ae91c3a4300f58
Nathan Shubert-Harbison <nathan@carterhales.com>
2016-01-18 10:50:10 -0800

    removed extend_args, should use wp_parse_args instead

--- includes/utility.php
+++ includes/utility.php
@@ -50,34 +50,6 @@ function pre_dump($var = false) {
 
 
 /**
- * Extend default arguments. Allows you to pass associative arrays to template functions with defaults,
- * and merge in with passed in args.
- *
- * @package d7
- * @subpackage utility
- *
- * @param array $args 	Associative array of arguments to merge
- *
- * @link http://gabrieleromanato.name/php-using-associative-arrays-to-handle-default-function-arguments/
- *
- */
-function extend_args($args, $defaults = array()) {
-
-	if ( is_object( $args ) ) {
-		$result = get_object_vars( $args );
-	} elseif ( is_array( $args ) ) {
-		$result =& $args;
-	}
-
-	if ( is_array( $defaults ) ) {
-		return array_merge( $defaults, $result );
-	}
-
-	return $result;
-}
-
-
-/**
  * Check if the site is a local development site. Useful for excluding Google Analytics on local.
  *
  * @package d7
